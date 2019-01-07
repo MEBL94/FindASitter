aa1375924d06475ee21d04e9bc4ac77eb943f314
Mathias Emil Blad Larsen <mebl94@gmail.com>
2019-01-07 13:00:54 +0100

    My first commit

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://angular2api1.azurewebsites.net/api/internships2' // not an actual api endpoint.
-  //This is just to illustrate env. variables. 
-};
